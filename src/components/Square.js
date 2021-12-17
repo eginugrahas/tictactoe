71962d2b1476864e08b3138f2e16ce33ed8470e0
eginugrahas <eginugrahasmart@gmail.com>
2021-12-17 13:14:03 +0700

    finished base game

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,4 +1,18 @@
-import React from 'react'
+import React, { Component } from 'react'
+
+// export default class square extends Component {
+//     render() {
+//         return (
+//             <button className="square" 
+//                 onClick={() => this.props.onClick()}
+//             >
+//                {this.props.value}
+//             </button>
+//         )
+//     }
+// }
+
+/* Changing from class to function components */ 
 
 export default function Square(props) {
     return (
